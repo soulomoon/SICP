7cf183d1e58d7f82bde938d225de92c190f724a4
soulomoon <fwy996602672@gmail.com>
2016-11-05 20:31:00 +0800

    1.34

---
+++ Exercise 1.34.scm
@@ -0,0 +1,13 @@
+; Exercise 1.34: Suppose we define the procedure
+
+; (define (f g) (g 2))
+; Then we have
+
+; (f square)
+; 4
+
+; (f (lambda (z) (* z (+ z 1))))
+; 6
+; What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain.
+
+something went wrong
