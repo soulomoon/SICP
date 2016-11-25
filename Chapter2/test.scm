f49b267fd96a5127ee04a9716374241bc368b4b5
soulomoon <fwy996602672@gmail.com>
2016-11-25 17:27:32 +0800

    test

--- Chapter2/test.scm
+++ Chapter2/test.scm
@@ -1,32 +1,10 @@
-(define (entry tree) (car tree))
-(define (left-branch tree) (cadr tree))
-(define (right-branch tree) (caddr tree))
-(define (make-tree entry left right)
-  (list entry left right))
-
-  (define (element-of-set? x set)
-  (cond ((null? set) false)
-        ((= x (entry set)) true)
-        ((< x (entry set))
-         (element-of-set? 
-          x 
-          (left-branch set)))
-        ((> x (entry set))
-         (element-of-set? 
-          x 
-          (right-branch set)))))
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+sudo apt-get install build-essential autoconf libtool libssl-dev
+./configure && make
+make install
 
 
-(define (adjoin-set x set)
-  (cond ((null? set) (make-tree x '() '()))
-        ((= x (entry set)) set)
-        ((< x (entry set))
-         (make-tree 
-          (entry set)
-          (adjoin-set x (left-branch set))
-          (right-branch set)))
-        ((> x (entry set))
-         (make-tree
-          (entry set)
-          (left-branch set)
-          (adjoin-set x (right-branch set))))))
+
+vultr:
+9vM.22qTG[92{4gf
