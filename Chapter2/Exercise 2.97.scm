0fe8cd31d7c32f4bb82977fbe4fd265f0e227108
soulomoon <fwy996602672@gmail.com>
2016-12-07 17:59:54 +0800

    2.97 add test

--- Chapter2/Exercise 2.97.scm
+++ Chapter2/Exercise 2.97.scm
@@ -543,6 +543,18 @@
 (display (reduce 4 2))(newline)
 (display (make-rational a b))(newline)
 (display (make-rational 4 2))(newline)
+
+(define p1 
+  (make-polynomial 'x '((1 1) (0 1))))
+(define p2 
+  (make-polynomial 'x '((3 1) (0 -1))))
+(define p3 
+  (make-polynomial 'x '((1 1))))
+(define p4 
+  (make-polynomial 'x '((2 1) (0 -1))))
+(define rf1 (make-rational p1 p2))
+(define rf2 (make-rational p3 p4))
+(display (add rf1 rf2))
 ; Welcome to DrRacket, version 6.7 [3m].
 ; Language: SICP (PLaneT 1.18); memory limit: 128 MB.
 ; 'install_transform_done
@@ -554,4 +566,5 @@
 ; (2 1)
 ; (rational (polynomial x ((2 1) (1 1) (0 1))) polynomial x ((0 1)))
 ; (rational 2 . 1)
+; (rational (polynomial x ((4 1) (3 1) (2 1) (1 -2) (0 -1))) polynomial x ((5 1) (3 -1) (2 -1) (0 1)))
 ; > 
