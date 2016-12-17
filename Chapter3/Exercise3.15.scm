840d69d7d433c74b737bc174f9b11edbff67904f
soulomoon <fwy996602672@gmail.com>
2016-12-17 22:23:02 +0800

    3.15

---
+++ Chapter3/Exercise3.15.scm
@@ -0,0 +1,20 @@
+; Exercise 3.15: Draw box-and-pointer diagrams to explain the effect of set-to-wow! on the structures z1 and z2 above.
+
+
+
+Z1-->[*][*]
+      |  |	
+      V  V
+ x-->[*][*]->[*][/]
+      |       |	
+      V       V
+      'wow    b
+               
+                  
+Z2-->[*][*]-->[*][*]-->[*][/]
+      |        v        v
+      |                 b
+      |                 ^
+      +------>[*][*]-->[*][/]
+               v
+               'wow
