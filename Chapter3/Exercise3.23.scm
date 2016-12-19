8961d457512ee4f978d01af20bf81ced47c63b05
soulomoon <fwy996602672@gmail.com>
2016-12-19 17:45:00 +0800

    3.23 done

--- Chapter3/Exercise3.23.scm
+++ Chapter3/Exercise3.23.scm
@@ -1,87 +1,26 @@
 ; Exercise 3.23: A deque (“double-ended queue”) is a sequence in which items can be inserted and deleted at either the front or the rear. Operations on deques are the constructor make-deque, the predicate empty-deque?, selectors front-deque and rear-deque, and mutators front-insert-deque!, rear-insert-deque!, front-delete-deque!, rear-delete-deque!. Show how to represent deques using pairs, and give implementations of the operations.151 All operations should be accomplished in Θ(1)Θ(1) steps.
 
-(define (make-queue)
-  (let ((front-ptr '())
-        (rear-ptr '()))
 
-    (define (set-front-ptr! i)
-      (set! front-ptr i))
-    (define (set-rear-ptr! i)
-      (set! rear-ptr i))
-    (define (empty-queue?) 
-      (null? front-ptr))
+; to enable for backward rear delete. we have two way pine
 
-    (define (front-queue)
-      (if (empty-queue?)
-          (error "FRONT called with an 
-                  empty queue" front-ptr)
-          (car front-ptr)))
-    
-    (define (rear-deque)
-      (if (empty-queue?)
-          (error "REAR called with an 
-                  empty queue" front-ptr)
-          (car rear-ptr)))
-
-
-    (define (front-insert-queue! item)
-      (let ((new-pair (cons item '())))
-        (cond ((empty-queue?)
-              (set-front-ptr! new-pair)
-              (set-rear-ptr! new-pair)
-              dispatch)
-              (else (set! front-ptr 
-                          (set-cdr! new-pair
-                                    front-ptr)
-                    dispatch))))
-
-    (define (rear-insert-queue! item)
-      (let ((new-pair (cons item '())))
-        (cond ((empty-queue?)
-              (set-front-ptr! new-pair)
-              (set-rear-ptr! new-pair)
-              dispatch)
-              (else (set-cdr! rear-ptr 
-                              new-pair)
-                    (set-rear-ptr! new-pair)
-                    dispatch))))
-
-
-    (define (front-delete-queue!)
-      (cond ((empty-queue?)
-            (error "DELETE! called with 
-                    an empty queue" front-ptr))
-            (else (set-front-ptr!  
-                    (cdr front-ptr))
-                  dispatch)))
-    (define (rear-delete-queue!)
-      (cond ((empty-queue?)
-            (error "DELETE! called with 
-                    an empty queue" front-ptr))
-            (else (set-front-ptr!  
-                    (cdr front-ptr))
-                  dispatch)))
-    
-    
-    (define (dispatch m)
-      (cond
-        ((eq? m 'front-ptr) front-ptr)
-        ((eq? m 'rear-ptr) rear-ptr)
-        ((eq? m 'set-front-ptr!) set-front-ptr!)
-        ((eq? m 'set-rear-ptr!) set-rear-ptr!)
-        (else (error "unknow"))
-      )    
-    )
-    dispatch))
-
-
+;    +------------+   +---------+
+;    |            |   |         |
+;    |           [*] [*]        |
+;    |              ^           |    
+;    v              |           v
+; [*] [*]      [*] [*]       [*] [*]
+;  |   |        | ^           |   |
+;  a   v        b |           c   v
+;   [/] [*]       |            [*] [/]
+;        |        |             | 
+;        +--------+-------------+
 
 (define (front-ptr queue) (car queue))
 (define (rear-ptr queue) (cdr queue))
 (define (set-front-ptr! queue item) 
-  (set-car! (cdr queue) item))
+  (set-car! queue item))
 (define (set-rear-ptr! queue item) 
-  (set-cdr! (cdr queue) item))
+  (set-cdr! queue item))
 
 (define (empty-queue? queue) 
   (null? (front-ptr queue)))
@@ -95,23 +34,104 @@
               empty queue" queue)
       (car (front-ptr queue))))
 
-(define (insert-queue! queue item)
+(define (rear-insert-queue! queue item)
   (let ((new-pair (cons item (cons '() '()))))
     (cond ((empty-queue? queue)
            (set-front-ptr! queue new-pair)
            (set-rear-ptr! queue new-pair)
            queue)
-          (else (set-cdr! (rear-ptr queue) 
+          (else (set-car! (cdr new-pair)
+                          (rear-ptr queue))
+                (set-cdr! (cdr (rear-ptr queue)) 
                           new-pair)
                 (set-rear-ptr! queue new-pair)
                 queue))))
 
+(define (front-insert-queue! queue item)
+  (let ((new-pair (cons item (cons '() '()))))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (cdr new-pair)
+                          (front-ptr queue))
+                (set-car! (cdr (front-ptr queue)) 
+                          new-pair)
+                (set-front-ptr! queue new-pair)
+                queue))))
 
-(define (delete-queue! queue)
+
+(define (front-delete-queue! queue)
   (cond ((empty-queue? queue)
          (error "DELETE! called with 
                  an empty queue" queue))
         (else (set-front-ptr! 
                queue 
-               (cdr (front-ptr queue)))
+               (cddr (front-ptr queue)))
               queue)))
+
+(define (rear-delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with 
+                 an empty queue" queue))
+        (else (set-rear-ptr!
+               queue 
+               (cadr (rear-ptr queue)))
+              (set-cdr! (cdr (rear-ptr queue)) nil)
+              queue)))
+
+
+
+
+(define (print_queue q)
+  (define (iter q)
+    (if (null? q)
+        nil
+        (cons (car q) (iter (cddr q)))))
+  (display (iter (front-ptr q)))(newline))
+
+(define q1 (make-queue))
+
+(rear-insert-queue! q1 'a)
+(print_queue q1)
+
+(rear-insert-queue! q1 'b)
+(print_queue q1)
+
+(rear-insert-queue! q1 'c)
+(print_queue q1)
+
+(front-insert-queue! q1 'd)
+(print_queue q1)
+
+(rear-delete-queue! q1)
+(print_queue q1)
+
+(front-delete-queue! q1)
+(print_queue q1)
+
+(front-delete-queue! q1)
+(print_queue q1)
+
+(front-delete-queue! q1)
+(print_queue q1)
+
+; Welcome to DrRacket, version 6.7 [3m].
+; Language: SICP (PLaneT 1.18); memory limit: 128 MB.
+; {mcons {mcons 'a {mcons '() '()}} {mcons 'a {mcons '() '()}}}
+; (a)
+; {mcons #0={mcons 'a {mcons '() #1={mcons 'b {mcons #0# '()}}}} #1#}
+; (a b)
+; {mcons #0={mcons 'a {mcons '() #1={mcons 'b {mcons #0# #2={mcons 'c {mcons #1# '()}}}}}} #2#}
+; (a b c)
+; {mcons #0={mcons 'd {mcons '() #1={mcons 'a {mcons #0# #2={mcons 'b {mcons #1# #3={mcons 'c {mcons #2# '()}}}}}}}} #3#}
+; (d a b c)
+; {mcons #0={mcons 'd {mcons '() #1={mcons 'a {mcons #0# #2={mcons 'b {mcons #1# '()}}}}}} #2#}
+; (d a b)
+; {mcons #0={mcons 'a {mcons {mcons 'd {mcons '() #0#}} #1={mcons 'b {mcons #0# '()}}}} #1#}
+; (a b)
+; {mcons #0={mcons 'b {mcons #1={mcons 'a {mcons {mcons 'd {mcons '() #1#}} #0#}} '()}} #0#}
+; (b)
+; {mcons '() #0={mcons 'b {mcons #1={mcons 'a {mcons {mcons 'd {mcons '() #1#}} #0#}} '()}}}
+; ()
+; > 
