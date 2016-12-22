7972f4e7423c11d7ed287e5d5db52b0dab620275
soulomoon <fwy996602672@gmail.com>
2016-12-22 23:16:56 +0800

    update signal.scm

---
+++ Chapter3/Exercise3.31.scm
@@ -0,0 +1,32 @@
+; Exercise 3.31: The internal procedure accept-action-procedure! defined in make-wire specifies that when a new action procedure is added to a wire, the procedure is immediately run. Explain why this initialization is necessary. In particular, trace through the half-adder example in the paragraphs above and say how the system’s response would differ if we had defined accept-action-procedure! as
+
+(define (accept-action-procedure! proc)
+  (set! action-procedures 
+        (cons proc action-procedures)))
+
+(define (make-wire)
+  (let ((signal-value 0) 
+        (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each 
+                  action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures 
+            (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) 
+             signal-value)
+            ((eq? m 'set-signal!) 
+             set-my-signal!)
+            ((eq? m 'add-action!) 
+             accept-action-procedure!)
+            (else (error "Unknown operation: 
+                          WIRE" m))))
+    dispatch))
+
+
+;if not being run immediately, then the change effect to a wire won't appear until that particular wire being set to a new signal
--- Chapter3/queue.scm
+++ Chapter3/queue.scm
@@ -37,3 +37,57 @@
                queue 
                (cdr (front-ptr queue)))
               queue)))
+
+
+
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) 
+  (car (segments agenda)))
+(define (rest-segments agenda) 
+  (cdr (segments agenda)))
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time 
+           (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! 
+         (segment-queue (car segments))
+         action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment 
+                      time 
+                      action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment 
+                time 
+                action)
+               segments))
+        (add-to-segments! segments))))
