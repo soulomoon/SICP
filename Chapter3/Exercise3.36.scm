573b040f60252b85feea74321ed2f17a8403b6be
soulomoon <fwy996602672@gmail.com>
2016-12-24 15:22:03 +0800

    3.36

---
+++ Chapter3/Exercise3.36.scm
@@ -0,0 +1,43 @@
+; Exercise 3.36: Suppose we evaluate the following sequence of expressions in the global environment:
+
+; (define a (make-connector))
+; (define b (make-connector))
+; (set-value! a 10 'user)
+; At some time during evaluation of the set-value!, the following expression from the connector’s local procedure is evaluated:
+
+; (for-each-except 
+;   setter inform-about-value constraints)
+; Draw an environment diagram showing the environment in which the above expression is evaluated.
+  +-----------+
+--|           |
+  +-----------+
+          ^
+          |
+        +-----------------------------+<------+
+        |loop------------------------------->[*] [*]----> parameters:list
+        |exception:'user              |                   body:....
+        |procedure:inform-about-value |
+        |list:'()                     |
+        |                             |
+        +-----------------------------+
+           ^
+           |
+        +-----------+
+        |items:false|          
+        +-----------+
+         (cond ((null? '()  ) 'done)
+          ((eq? (car   '()) 'user) 
+           (loop (cdr '())))
+          (else (procedure (car '()  ))
+                (loop (cdr '()  )))) 
+
+(define (outside value) (+ value x))
+(define x 1)
+(define (runner)
+  (define x 2)
+  (ouside 1)
+)
+
+
+
+
