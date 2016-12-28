c8dc2a099a598442c70decd6987ad0f82f2e66d9
soulomoon <fwy996602672@gmail.com>
2016-12-28 21:12:29 +0800

    3.66 modify

--- Chapter3/Exercise3.66.scm
+++ Chapter3/Exercise3.66.scm
@@ -1,9 +1,9 @@
 ; Exercise 3.66: Examine the stream (pairs integers integers). Can you make any general comments about the order in which the pairs are placed into the stream? For example, approximately how many pairs precede the pair (1, 100)? the pair (99, 100)? the pair (100, 100)? (If you can make precise mathematical statements here, all the better. But feel free to give more qualitative answers if you find yourself getting bogged down.)
 
 (1, 100) preceding first there are (1, 1) to (1, 99)
-total 99, then observing the interleave, there would be 99 with form of (n, m),
+total 99, then observing the interleave, there would be 98 with form of (n, m),
 where n > 1, m < 99.
-total 199
+total 197
 with total of m pick, 
 approximately.
 1/2:(1, n)
@@ -14,7 +14,7 @@ because the first is (1, 1)
 and there are not coresponding (2, 1)
 so for every (n, n) there are exceptions
 so the total preceding pairs:
-for (1, 100): 99*2 + 1 = 199
+for (1, 100): 99*2 + 1 = 197
 for (99, 100): 99*2^99 + 99
 for (100, 100): 99*2^100 + 99
 for (k, n): n*2^k + k (if k = n, you minus 1)
