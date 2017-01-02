1c508703f600f7189eb82143c9d71e77e98fdd5d
soulomoon <fwy996602672@gmail.com>
2017-01-02 22:58:49 +0800

    4.01 modify

--- Chapter4/Exercise4.01.scm
+++ Chapter4/Exercise4.01.scm
@@ -12,22 +12,22 @@
              
 ; left to right
 (define (list-of-values exps env)
-  (let ((first (eval (first-operand exps) env)))
     (if (no-operands? exps)
         '()
-        (cons first
-              (list-of-values 
-              (rest-operands exps) 
-              env)))))
+        (let ((first (eval (first-operand exps) env)))
+              (cons first
+                    (list-of-values 
+                    (rest-operands exps) 
+                    env)))))
 ; right to left
 (define (list-of-values exps env)
-  (let ((rest (list-of-values 
-                (rest-operands exps) 
-                env)))
     (if (no-operands? exps)
         '()
-        (cons (eval (first-operand exps) env)
-              (list-of-values 
-              (rest-operands exps) 
-              env)))))
+        (let ((rest (list-of-values 
+                      (rest-operands exps) 
+                      env)))
+              (cons (eval (first-operand exps) env)
+                    (list-of-values 
+                    (rest-operands exps) 
+                    env)))))
 
