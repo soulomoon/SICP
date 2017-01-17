b455ae53f525ea599c2d5740ac210ca212e9304f
soulomoon <fwy996602672@gmail.com>
2017-01-18 02:15:05 +0800

    4.24 modified

--- Chapter4/Exercise4.24.scm
+++ Chapter4/Exercise4.24.scm
@@ -1,41 +1,56 @@
 ; Exercise 4.24: Design and carry out some experiments to compare the speed of the original metacircular evaluator with the version in this section. Use your results to estimate the fraction of time that is spent in analysis versus execution for various procedures.
-(load "/Users/soulomoon/git/SICP/Chapter4/separating.scm")
+(load "/Users/soulomoon/git/SICP/Chapter4/ex4.03.rkt")
+
 (define (report start_time)
   (display " *** ")
   (display (- (runtime) start_time))
   (newline ))
 
-(define (eval# exp env) 
+
+
+(define a 
+'(begin 
+    (define (factorial n)
+          (if (< n 2)
+              1
+(* (factorial (- n 1)) n)))
+    (define (fib n)
+          (cond ((= n 0) 0)
+                ((= n 1) 1)
+                (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+(factorial 50)
+(fib 20)
+))
+
+(define
+  (runtime-interpret exp) 
   (let ((start_time (runtime)))
-        ; (report start_time)
-        (let ((analyzed (analyze exp)))
-              (report start_time)
-              (display (analyzed env))
-              (report start_time))))
+    (display (interpret exp))
+    (report start_time)
+  ))
 
+(newline )(display "without separating")(newline )
+(runtime-interpret a)
+(runtime-interpret a)
+(runtime-interpret a)
+(load "/Users/soulomoon/git/SICP/Chapter4/separating.scm")
+(newline )(display "separating")(newline )
+(runtime-interpret a)
+(runtime-interpret a)
+(runtime-interpret a)
 
-(define a '(begin
-  (define (test)
-  (define b 2)
-  (define (c) 3)
-  (+ b (c)))
-(test)))
-(define e '(+ 1 1))
-(define b '(- 2 2))
-
-; (interpret a)
-(interpret e)
-(interpret b)
-(interpret a)
-
-; eval is about a 2 3 times longer than analysis
-
+; took about half of the time when separating eval and analysis
 ; Welcome to DrRacket, version 6.7 [3m].
 ; Language: SICP (PLaneT 1.18); memory limit: 128 MB.
-;  *** 684
-; 2 *** 1473
-;  *** 63
-; 0 *** 189
-;  *** 286
-; 5 *** 646
+
+; without separating
+; 6765 *** 622443
+; 6765 *** 620444
+; 6765 *** 603262
+
+; separating
+; 6765 *** 351626
+; 6765 *** 273320
+; 6765 *** 345168
 ; > 
