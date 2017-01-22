feab2cc97f4edb01722051c0700a39e510d3b602
soulomoon <fwy996602672@gmail.com>
2017-01-23 02:05:24 +0800

    4.39

---
+++ Chapter4/Exercise4.39.scm
@@ -0,0 +1,64 @@
+; Exercise 4.39: Does the order of the restrictions in the multiple-dwelling procedure affect the answer? Does it affect the time to find an answer? If you think it matters, demonstrate a faster program obtained from the given one by reordering the restrictions. If you think it does not matter, argue your case.
+
+#lang swindle
+
+(define (square x) (* x x))
+(define (require p)
+  (if (not p) (amb)))
+(define (an-integer-between low high)
+  (require (<= low high))
+  (amb low (an-integer-between (+ low 1) high)))
+
+(define (distinct? items)
+  (cond ((null? items) true)
+        ((null? (cdr items)) true)
+        ((member (car items) (cdr items)) false)
+        (else (distinct? (cdr items)))))
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    ; 24/625 
+    (require
+     (distinct? (list baker cooper fletcher 
+                      miller smith)))
+    ; 1/2
+    (require (> miller cooper))
+    ; 1/5
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    ; 1/5
+    (require
+     (not (= (abs (- smith fletcher)) 1)))
+    ; 1/5
+    (require 
+     (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
+(define (test-time n)
+  (let ((starttime 0)
+        (result 0))
+    (define (iter n)
+      (if (< n 0)
+          result
+          (begin
+            (set! starttime (runtime))
+            (amb-collect (multiple-dwelling))
+            (set! result (+ result (report starttime)))
+            (iter (- n 1)))))
+    (iter n)))
+(display (test-time 200))
+
+; it does not effect the answer
+; and it does effect the speed
+; if each require take about the same time. the above would be the bestsolution
+; here we should see distinct is o(n^2) others are o(1), but since there are only 5 of them , it does not make a huge difference, and the times it takes for the first is depend on the number of the list, 
+; and each require would effect the latter require to succed or not, so it is actually a harder math problem, which i wish not spending too much time on it.
