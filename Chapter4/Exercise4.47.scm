8a526938575cb5ff15837b316d6022eb9ee72198
soulomoon <fwy996602672@gmail.com>
2017-01-28 01:07:17 +0800

    4.47

---
+++ Chapter4/Exercise4.47.scm
@@ -0,0 +1,12 @@
+; Exercise 4.47: Louis Reasoner suggests that, since a verb phrase is either a verb or a verb phrase followed by a prepositional phrase, it would be much more straightforward to define the procedure parse-verb-phrase as follows (and similarly for noun phrases):
+
+; (define (parse-verb-phrase)
+;   (amb (parse-word verbs)
+;        (list 
+;         'verb-phrase
+;         (parse-verb-phrase)
+;         (parse-prepositional-phrase))))
+; Does this work? Does the program’s behavior change if we interchange the order of expressions in the amb?
+
+it is an infinite loop - -// (eval from left to right)
+if it is from right to left, it can stop
