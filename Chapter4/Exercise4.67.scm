42a20afbd1d2e32c682d8c9d5c219800a54ff3af
soulomoon <fwy996602672@gmail.com>
2017-02-05 23:55:59 +0800

    4.67

---
+++ Chapter4/Exercise4.67.scm
@@ -0,0 +1,3 @@
+; Exercise 4.67: Devise a way to install a loop detector in the query system so as to avoid the kinds of simple loops illustrated in the text and in Exercise 4.64. The general idea is that the system should maintain some sort of history of its current chain of deductions and should not begin processing a query that it is already working on. Describe what kind of information (patterns and frames) is included in this history, and how the check should be made. (After you study the details of the query-system implementation in 4.4.4, you may want to modify the system to include your loop detector.)
+
+; if is has the same patterns and frams as in the history, it would be infinite loop.
