60721a9ebb65fd0f5a18c4f6d7a61ed1f11020fe
soulomoon <fwy996602672@gmail.com>
2017-02-05 23:48:39 +0800

    4.66

--- Chapter4/Exercise4.66.scm
+++ Chapter4/Exercise4.66.scm
@@ -10,3 +10,7 @@
 ; where accumulation-function can be things like sum, average, or maximum. Ben reasons that it should be a cinch to implement this. He will simply feed the query pattern to qeval. This will produce a stream of frames. He will then pass this stream through a mapping function that extracts the value of the designated variable from each frame in the stream and feed the resulting stream of values to the accumulation function. Just as Ben completes the implementation and is about to try it out, Cy walks by, still puzzling over the wheel query result in Exercise 4.65. When Cy shows Ben the system’s response, Ben groans, “Oh, no, my simple accumulation scheme won’t work!”
 
 ; What has Ben just realized? Outline a method he can use to salvage the situation.
+
+; there would be duplicate.
+; way to solve it is to eliminate the duplicate.
+; that he can keep track on the a frame of variable that has already be in the accumulation, and ignore duplicate.
