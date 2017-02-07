b01d1ec2eb3819aa29e695514d8b3b06ea7fc556
soulomoon <fwy996602672@gmail.com>
2017-02-07 17:11:33 +0800

    4.70

---
+++ Chapter4/Exercise4.70.md
@@ -0,0 +1,32 @@
+; Exercise 4.70: What is the purpose of the let bindings in the procedures add-assertion! and add-rule!? What would be wrong with the following implementation of add-assertion!? Hint: Recall the definition of the infinite stream of ones in 3.5.2: (define ones (cons-stream 1 ones)).
+```
+(define (add-assertion! assertion)
+  (store-assertion-in-index assertion)
+  (set! THE-ASSERTIONS
+        (cons-stream assertion 
+                     THE-ASSERTIONS))
+  'ok)
+```
+for cons-stream, the later var is delayed
+
+for ones definition, it would loopingly interpret ones, when cdring the stream list
+
+same is true for under
+```
+  (set! THE-ASSERTIONS
+        (cons-stream assertion 
+                     THE-ASSERTIONS))
+```
+the  '(let ((old-assertions THE-ASSERTIONS))' here 
+let old-assertions take the value of THE-ASSERTIONS.
+would strip out the outer layer which is a pointer
+
+same here:
+```
+(define a 1)
+(define b a)
+(set! a 2)
+b
+```
+as you can see, b would change with a even if a new value has
+been assign to a.
--- Chapter4/query-system.scm
+++ Chapter4/query-system.scm
@@ -280,3 +280,86 @@
    (get-stream (index-key-of pattern)
                'rule-stream)
    (get-stream '? 'rule-stream)))
+
+
+(define (add-rule-or-assertion! assertion)
+  (if (rule? assertion)
+      (add-rule! assertion)
+      (add-assertion! assertion)))
+
+(define (add-assertion! assertion)
+  (store-assertion-in-index assertion)
+  (let ((old-assertions THE-ASSERTIONS))
+    (set! THE-ASSERTIONS
+          (cons-stream assertion 
+                       old-assertions))
+    'ok))
+
+(define (add-rule! rule)
+  (store-rule-in-index rule)
+  (let ((old-rules THE-RULES))
+    (set! THE-RULES
+          (cons-stream rule old-rules))
+    'ok))
+
+(define (store-assertion-in-index assertion)
+  (if (indexable? assertion)
+      (let ((key (index-key-of assertion)))
+        (let ((current-assertion-stream
+               (get-stream 
+                key 'assertion-stream)))
+          (put key
+               'assertion-stream
+               (cons-stream 
+                assertion
+                current-assertion-stream))))))
+
+(define (store-rule-in-index rule)
+  (let ((pattern (conclusion rule)))
+    (if (indexable? pattern)
+        (let ((key (index-key-of pattern)))
+          (let ((current-rule-stream
+                 (get-stream 
+                  key 'rule-stream)))
+            (put key
+                 'rule-stream
+                 (cons-stream 
+                  rule
+                  current-rule-stream)))))))
+
+(define (store-assertion-in-index assertion)
+  (if (indexable? assertion)
+      (let ((key (index-key-of assertion)))
+        (let ((current-assertion-stream
+               (get-stream 
+                key 'assertion-stream)))
+          (put key
+               'assertion-stream
+               (cons-stream 
+                assertion
+                current-assertion-stream))))))
+
+(define (store-rule-in-index rule)
+  (let ((pattern (conclusion rule)))
+    (if (indexable? pattern)
+        (let ((key (index-key-of pattern)))
+          (let ((current-rule-stream
+                 (get-stream 
+                  key 'rule-stream)))
+            (put key
+                 'rule-stream
+                 (cons-stream 
+                  rule
+                  current-rule-stream)))))))
+                  
+(define (indexable? pat)
+  (or (constant-symbol? (car pat))
+      (var? (car pat))))
+
+(define (index-key-of pat)
+  (let ((key (car pat)))
+    (if (var? key) '? key)))
+
+(define (use-index? pat)
+  (constant-symbol? (car pat)))
+
