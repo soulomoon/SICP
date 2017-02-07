2aeecaa94a08c0a578430638be8ca1c5e0b0aaab
soulomoon <fwy996602672@gmail.com>
2017-02-07 15:34:13 +0800

    ; 4.4.4.5Maintaining the Data Base

--- Chapter4/query-system.scm
+++ Chapter4/query-system.scm
@@ -1,3 +1,4 @@
+; 4.4.4.1The Driver Loop and Instantiation
 (define input-prompt  ";;; Query input:")
 (define output-prompt ";;; Query results:")
 (define (query-driver-loop)
@@ -42,13 +43,14 @@
           (else exp)))
   (copy exp))
 
+; 4.4.4.2The Evaluator
 (define (qeval query frame-stream)
   (let ((qproc (get (type query) 'qeval)))
     (if qproc
         (qproc (contents query) frame-stream)
         (simple-query query frame-stream))))
 
-
+; Simple queries
 (define (simple-query query-pattern 
                       frame-stream)
   (stream-flatmap
@@ -59,6 +61,7 @@
         (apply-rules query-pattern frame))))
    frame-stream))
 
+; Compound queries
 (define (conjoin conjuncts frame-stream)
   (if (empty-conjunction? conjuncts)
       frame-stream
@@ -79,6 +82,7 @@
                frame-stream)))))
 (put 'or 'qeval disjoin)
 
+; Filters
 (define (negate operands frame-stream)
   (stream-flatmap
    (lambda (frame)
@@ -111,7 +115,7 @@
   frame-stream)
 (put 'always-true 'qeval always-true)
 
-
+; 4.4.4.3Finding Assertions by Pattern Matching
 (define (find-assertions pattern frame)
   (stream-flatmap 
     (lambda (datum) 
@@ -147,3 +151,132 @@
         (pattern-match 
          (binding-value binding) dat frame)
         (extend var dat frame))))
+
+; 4.4.4.4Rules and Unification
+(define (apply-rules pattern frame)
+  (stream-flatmap 
+   (lambda (rule)
+     (apply-a-rule rule pattern frame))
+   (fetch-rules pattern frame)))
+
+(define (apply-a-rule rule
+                      query-pattern
+                      query-frame)
+  (let ((clean-rule 
+         (rename-variables-in rule)))
+    (let ((unify-result
+           (unify-match query-pattern
+                        (conclusion clean-rule)
+                        query-frame)))
+      (if (eq? unify-result 'failed)
+          the-empty-stream
+          (qeval (rule-body clean-rule)
+                 (singleton-stream 
+                  unify-result))))))
+
+(define (rename-variables-in rule)
+  (let ((rule-application-id 
+         (new-rule-application-id)))
+    (define (tree-walk exp)
+      (cond ((var? exp)
+             (make-new-variable 
+              exp 
+              rule-application-id))
+            ((pair? exp)
+             (cons (tree-walk (car exp))
+                   (tree-walk (cdr exp))))
+            (else exp)))
+    (tree-walk rule)))
+
+(define (unify-match p1 p2 frame)
+  (cond ((eq? frame 'failed) 'failed)
+        ((equal? p1 p2) frame)
+        ((var? p1)
+         (extend-if-possible p1 p2 frame))
+        ((var? p2)
+         (extend-if-possible 
+          p2 
+          p1 
+          frame))        ; ***
+        ((and (pair? p1) 
+              (pair? p2))
+         (unify-match 
+          (cdr p1) 
+          (cdr p2)
+          (unify-match 
+           (car p1)
+           (car p2)
+           frame)))
+        (else 'failed)))
+
+(define (extend-if-possible var val frame)
+  (let ((binding (binding-in-frame var frame)))
+    (cond (binding
+           (unify-match
+            (binding-value binding) val frame))
+          ((var? val)                   ; ***
+           (let ((binding 
+                  (binding-in-frame 
+                   val
+                   frame)))
+             (if binding
+                 (unify-match
+                  var 
+                  (binding-value binding) 
+                  frame)
+                 (extend var val frame))))
+          ((depends-on? val var frame)  ; ***
+           'failed)
+          (else (extend var val frame)))))
+
+(define (depends-on? exp var frame)
+  (define (tree-walk e)
+    (cond ((var? e)
+           (if (equal? var e)
+               true
+               (let
+                 ((b (binding-in-frame 
+                      e 
+                      frame)))
+                  (if b
+                      (tree-walk 
+                       (binding-value b))
+                      false))))
+          ((pair? e)
+           (or (tree-walk (car e))
+               (tree-walk (cdr e))))
+          (else false)))
+  (tree-walk exp))
+
+; 4.4.4.5Maintaining the Data Base
+(define THE-ASSERTIONS the-empty-stream)
+
+(define (fetch-assertions pattern frame)
+  (if (use-index? pattern)
+      (get-indexed-assertions pattern)
+      (get-all-assertions)))
+
+(define (get-all-assertions) THE-ASSERTIONS)
+
+(define (get-indexed-assertions pattern)
+  (get-stream (index-key-of pattern)
+              'assertion-stream))
+
+(define (get-stream key1 key2)
+  (let ((s (get key1 key2)))
+    (if s s the-empty-stream)))
+
+(define THE-RULES the-empty-stream)
+
+(define (fetch-rules pattern frame)
+  (if (use-index? pattern)
+      (get-indexed-rules pattern)
+      (get-all-rules)))
+
+(define (get-all-rules) THE-RULES)
+
+(define (get-indexed-rules pattern)
+  (stream-append
+   (get-stream (index-key-of pattern)
+               'rule-stream)
+   (get-stream '? 'rule-stream)))
