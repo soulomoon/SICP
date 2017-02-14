75fdcaa8418e9b787f46c12f8d2966e528370946
soulomoon <fwy996602672@gmail.com>
2017-02-14 17:19:34 +0800

    4.79 pause

--- Chapter4/Exercise4.79.scm
+++ Chapter4/Exercise4.79.scm
@@ -11,6 +11,9 @@
 
 ; Implement for the query language a rule-application method that uses environments rather than renaming. See if you can build on your environment structure to create constructs in the query language for dealing with large systems, such as the rule analog of block-structured procedures. Can you relate any of this to the problem of making deductions in a context (e.g., “If I supposed that PP were true, then I would be able to deduce AA and BB.”) as a method of problem solving? (This problem is open-ended. A good answer is probably worth a Ph.D.)
 (load "/Users/soulomoon/git/SICP/Chapter4/ch4-query.rkt")
+
+; it seems if not include renaming proccess, it would be extreamly hard to avoid conflict 
+; shall I come back later when I am stronger
 ;;;SECTION 4.4.4.8
 ;;;Frames and bindings
 (define (make-binding variable value)
@@ -270,7 +273,105 @@
             ; (newline )
            'failed)
           (else (extend var val frame)))))
-          
+(define (unify-match p1 p2 frame)
+  (cond ((eq? frame 'failed) 'failed)
+        ((equal? p1 p2) frame)
+        ((var? p1) (extend-if-possible-frame p1 p2 frame))
+        ((var? p2) (extend-if-possible-layer p1 p2 frame)) ; {\em ; ***}
+        ((and (pair? p1) (pair? p2))
+         (unify-match (cdr p1)
+                      (cdr p2)
+                      (unify-match (car p1)
+                                   (car p2)
+                                   frame)))
+        (else 'failed)))
+
+(define (extend-if-possible-frame var val frame)
+  (let ((binding (binding-in-frame var frame))
+        (bindlayer (binding-up-frame var frame)))
+    (cond (binding
+           (unify-match
+            (binding-value binding) val frame))
+          (bindlayer
+            )
+          ((var? val)                     ; {\em ; ***}
+           (let ((binding (binding-in-frame val frame)))
+             (if binding
+                 (unify-match
+                  var (binding-value binding) frame)
+                 (extend var val frame))))
+          ((depends-on? val var frame)    ; {\em ; ***}
+           'failed)
+          (else (extend var val frame)))))
+
+(define (extend-if-possible-layer var val frame)
+  (let ((binding (binding-in-frame var frame)))
+    (cond (binding
+           (unify-match
+            (binding-value binding) val frame))
+          ((var? val)                     ; {\em ; ***}
+           (let ((binding (binding-in-frame val frame)))
+             (if binding
+                 (unify-match
+                  var (binding-value binding) frame)
+                 (extend var val frame))))
+          ((depends-on? val var frame)    ; {\em ; ***}
+           'failed)
+          (else (extend var val frame)))))
+
+(define (depends-on-frame? exp var frame)
+  (define (tree-walk e n)
+      (cond ((and (var? e) (= n 0))
+              (if (equal? var e)
+                  true
+                  (let ((b (binding-in-frame e frame))
+                        (by (binding-up-frame e frame)))
+                        (cond (and b by)
+                              (or (tree-walk (binding-value b) 1)
+                                  (tree-walk (car by) 0)))
+                              (b
+                                (tree-walk (binding-value b) 1))
+                              (by
+                                (tree-walk (car by) 0))
+                              (else false))))
+            ((var? e)
+              (let ((binding-layer (binding-down-layer e frame)))
+                      (cond ((binding-layer) 
+                              (tree-walk (car binding-layer) 1))
+                            (else false))))
+            ((pair? e)
+              (or (tree-walk (car e) n)
+                  (tree-walk (cdr e) n)))
+            (else false)))
+  (tree-walk exp 0))
+
+(define (depends-on-layer? exp var frame)
+  (define (tree-walk e n)
+      (cond 
+            ((and (var? e) (= n 1))
+              (if (equal? var e)
+                  true
+                  (let ((binding-layer (binding-down-layer e frame)))
+                          (cond ((binding-layer) 
+                                  (tree-walk (car binding-layer) 1))
+                                (else false))))
+                                  ))
+            ((var? e)
+                  (let ((b (binding-in-frame e frame))
+                            (by (binding-up-frame e frame)))
+                            (cond (and b by)
+                                  (or (tree-walk (binding-value b) 1)
+                                      (tree-walk (car by) 0)))
+                                  (b
+                                    (tree-walk (binding-value b) 1))
+                                  (by
+                                    (tree-walk (car by) 0))
+                                  (else false))
+            ((pair? e)
+              (or (tree-walk (car e) n)
+                  (tree-walk (cdr e) n)))
+            (else false)))
+  (tree-walk exp 1))
 ; (init-frame) as global
 (define (iq query)
   (let ((q (query-syntax-process query)))
--- test.scm
+++ test.scm
@@ -1,7 +1,99 @@
-(define time 2)
-(set! time 1)
+(define (unify-match p1 p2 frame)
+  (cond ((eq? frame 'failed) 'failed)
+        ((equal? p1 p2) frame)
+        ((var? p1) (extend-if-possible-frame p1 p2 frame))
+        ((var? p2) (extend-if-possible-layer p1 p2 frame)) ; {\em ; ***}
+        ((and (pair? p1) (pair? p2))
+         (unify-match (cdr p1)
+                      (cdr p2)
+                      (unify-match (car p1)
+                                   (car p2)
+                                   frame)))
+        (else 'failed)))
 
-(display time)
+(define (extend-if-possible-frame var val frame)
+  (let ((binding (binding-in-frame var frame))
+        (bindlayer (binding-up-frame var frame)))
+    (cond (binding
+           (unify-match
+            (binding-value binding) val frame))
+          (bindlayer
+            )
+          ((var? val)                     ; {\em ; ***}
+           (let ((binding (binding-in-frame val frame)))
+             (if binding
+                 (unify-match
+                  var (binding-value binding) frame)
+                 (extend var val frame))))
+          ((depends-on? val var frame)    ; {\em ; ***}
+           'failed)
+          (else (extend var val frame)))))
 
+(define (extend-if-possible-layer var val frame)
+  (let ((binding (binding-in-frame var frame)))
+    (cond (binding
+           (unify-match
+            (binding-value binding) val frame))
+          ((var? val)                     ; {\em ; ***}
+           (let ((binding (binding-in-frame val frame)))
+             (if binding
+                 (unify-match
+                  var (binding-value binding) frame)
+                 (extend var val frame))))
+          ((depends-on? val var frame)    ; {\em ; ***}
+           'failed)
+          (else (extend var val frame)))))
 
+(define (depends-on-frame? exp var frame)
+  (define (tree-walk e n)
+      (cond ((and (var? e) (= n 0))
+              (if (equal? var e)
+                  true
+                  (let ((b (binding-in-frame e frame))
+                        (by (binding-up-frame e frame)))
+                        (cond (and b by)
+                              (or (tree-walk (binding-value b) 1)
+                                  (tree-walk (car by) 0)))
+                              (b
+                                (tree-walk (binding-value b) 1))
+                              (by
+                                (tree-walk (car by) 0))
+                              (else false))))
+            ((var? e)
+              (let ((binding-layer (binding-down-layer e frame)))
+                      (cond ((binding-layer) 
+                              (tree-walk (car binding-layer) 1))
+                            (else false))))
+            ((pair? e)
+              (or (tree-walk (car e) n)
+                  (tree-walk (cdr e) n)))
+            (else false)))
+  (tree-walk exp 0))
 
+(define (depends-on-layer? exp var frame)
+  (define (tree-walk e n)
+      (cond 
+            ((and (var? e) (= n 1))
+              (if (equal? var e)
+                  true
+                  (let ((binding-layer (binding-down-layer e frame)))
+                          (cond ((binding-layer) 
+                                  (tree-walk (car binding-layer) 1))
+                                (else false))))
+                                  ))
+            ((var? e)
+                  (let ((b (binding-in-frame e frame))
+                            (by (binding-up-frame e frame)))
+                            (cond (and b by)
+                                  (or (tree-walk (binding-value b) 1)
+                                      (tree-walk (car by) 0)))
+                                  (b
+                                    (tree-walk (binding-value b) 1))
+                                  (by
+                                    (tree-walk (car by) 0))
+                                  (else false))
+            ((pair? e)
+              (or (tree-walk (car e) n)
+                  (tree-walk (cdr e) n)))
+            (else false)))
+  (tree-walk exp 1))
