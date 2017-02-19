cd897108d1f4bc73e132a7d822339387c0c7a851
Author soulomoon <fwy996602672@gmail.com>
2017-02-19 13:26:00 +0800

    5.02 format

--- Chapter5/Exercise5.02.scm
+++ Chapter5/Exercise5.02.scm
@@ -2,42 +2,37 @@
 (load "/Users/soulomoon/git/SICP/Chapter5/ch5-regsim.scm")
 (define factorial-machine
 (make-machine
-'(c p n)
-(list (list '+ +) (list '* *) (list '> >))
-'(
+  '(c p n)
+  (list (list '+ +) (list '* *) (list '> >))
+  '(
+    (assign c (const 1))
+    (assign p (const 1))
+  test->
+    (test (op >) (reg c) (reg n))
+    (branch (label factorial-done))
+    (assign p (op *) (reg c) (reg p))
+    (assign c (op +) (reg c) (const 1))
+    (goto (label test->))
+  factorial-done
+)))
 
-  (assign c (const 1))
-  (assign p (const 1))
-test->
-  (test (op >) (reg c) (reg n))
-  (branch (label factorial-done))
-  (assign p (op *) (reg c) (reg p))
-  (assign c (op +) (reg c) (const 1))
-  (goto (label test->))
-factorial-done
-
-)
-))
 (define factorial-machine1
 (make-machine
-'(c p r t n)
-(list (list '+ +) (list '* *) (list '> >))
-'(
-
-  (assign c (const 1))
-  (assign p (const 1))
-test-c
-  (test (op >) (reg c) (reg n))
-  (branch (label f-done))
-  (assign r (op *) (reg c) (reg p))
-  (assign p r)
-  (assign t (op +) (reg c) (const 1))
-  (assign c t)
-  (goto (label test-c))
-f-done
-
-)
-))
+  '(c p r t n)
+  (list (list '+ +) (list '* *) (list '> >))
+  '(
+    (assign c (const 1))
+    (assign p (const 1))
+  test-c
+    (test (op >) (reg c) (reg n))
+    (branch (label f-done))
+    (assign r (op *) (reg c) (reg p))
+    (assign p r)
+    (assign t (op +) (reg c) (const 1))
+    (assign c t)
+    (goto (label test-c))
+  f-done
+)))
 
 (set-register-contents! factorial-machine 'n 5)
 (start factorial-machine)
